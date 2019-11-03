9bfea1cfc98758c6277cee4d62fa4f20307e964a
heng-gezhizou <1873477865@qq.com>
2019-11-03 16:36:36 +0800

    拦截器

--- managersystem/src/main/java/com/suixingpay/student/component/MyWebMvcConfigurer.java
+++ managersystem/src/main/java/com/suixingpay/student/component/MyWebMvcConfigurer.java
@@ -2,7 +2,6 @@ package com.suixingpay.student.component;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
