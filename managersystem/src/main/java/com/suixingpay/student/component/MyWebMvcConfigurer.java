eb104c46769689c2fc6bae1ea6a9243c7779d3dd
heng-gezhizou <1873477865@qq.com>
2019-11-03 16:19:14 +0800

    拦截器

--- managersystem/src/main/java/com/suixingpay/student/component/MyWebMvcConfigurer.java
+++ managersystem/src/main/java/com/suixingpay/student/component/MyWebMvcConfigurer.java
@@ -17,12 +17,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public abstract class MyWebMvcConfigurer implements WebMvcConfigurer {
 
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/index.html").setViewName("login");
     }
-
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
